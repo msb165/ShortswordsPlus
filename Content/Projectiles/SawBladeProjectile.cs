e3da04898db13fb84de23250bc6c2d5bb1d49709
msb165 <matheusdesouzax2015@gmail.com>
2022-07-23 13:47:33 -0300

    Add a new weapon

---
+++ Content/Projectiles/SawBladeProjectile.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.ID;
+using System;
+
+namespace MoreShortswords.Content.Projectiles
+{
+    public class SawBladeProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Saw Blade"); 
+        }
+
+        public override void SetDefaults()
+        {          
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.DamageType = DamageClass.MeleeNoSpeed;
+            Projectile.ownerHitCheck = true;            
+            Projectile.ArmorPenetration = 10;
+            Projectile.aiStyle = -1;
+            Projectile.width = 56;
+            Projectile.height = 56;
+            Projectile.hide = true;
+        }
+
+        public override void AI()
+        {            
+            if (Projectile.soundDelay <= 0)
+            {
+                SoundEngine.PlaySound(SoundID.Item22, Projectile.position);
+                Projectile.soundDelay = 30;
+            }
+
+            Player player = Main.player[Projectile.owner];
+                       
+            Projectile.rotation += 0.4f * player.direction;
+            // Projectile.position.X = Projectile.position.X + (player.width / 2 * player.direction);            
+            Projectile.Center = player.MountedCenter + new Vector2(30f * player.direction, 0f);
+            Projectile.position.Y = player.position.Y;
+            Projectile.spriteDirection = player.direction;
+
+            player.itemTime = player.itemAnimation = 2;        
+            player.itemRotation = Projectile.rotation;
+            player.heldProj = Projectile.whoAmI;
+
+            if (Main.myPlayer == Projectile.owner && !player.controlUseItem)
+            {
+                Projectile.Kill();
+            }      
+
+            if (player.dead || !player.active)
+            {
+                Projectile.Kill();
+            }
+
+            SetVisualOffsets();
+        }
+
+        private void SetVisualOffsets()
+        {
+            int halfProjWidth = Projectile.width / 2;
+            int halfProjHeight = Projectile.height / 2;
+
+            DrawOriginOffsetX = 0;
+            DrawOffsetX = -((56 / 2) - halfProjWidth);
+            DrawOriginOffsetY = -((56 / 2) - halfProjHeight);
+        }
+
+    }
+}
---
+++ Content/Weapons/SawBlade.cs
@@ -0,0 +1,58 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using MoreShortswords.Content.Projectiles;
+
+
+namespace MoreShortswords.Content.Weapons
+{
+    public class SawBlade : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Saw Blade");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 56;
+            Item.height = 56;
+
+            Item.damage = 12;
+            Item.DamageType = DamageClass.MeleeNoSpeed;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+
+            Item.value = Item.sellPrice(0, 0, 15, 9);
+            Item.rare = ItemRarityID.Blue;         
+
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+
+
+            Item.channel = true;
+            Item.autoReuse = true;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+
+            Item.shoot = ModContent.ProjectileType<SawBladeProjectile>();
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+           return player.ownedProjectileCounts[Item.shoot] <= 1;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.PlatinumBar, 6)
+                .AddIngredient(ItemID.SilverBar, 6)
+                .AddIngredient(ItemID.HellstoneBar, 6)
+                .AddTile(TileID.Hellforge)
+                .Register();
+        }
+    }
+}
